384fe61371bea22f07a908ee2b5ee2c0bee9b196
Alishta <arnery1234@gmail.com>
2024-04-08 13:01:47 +0300

    hw_40 order form

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header/Header'
-import Menu from './pages/Menu'
-import { Route, Routes, NavLink } from 'react-router-dom'
-import Auth from './pages/Auth'
-import NotFound from './pages/NotFound'
-import Home from './pages/Home'
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Menu from './pages/Menu';
+import { Route, Routes, NavLink } from 'react-router-dom';
+import Auth from './pages/Auth';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import NewOrder from './pages/NewOrder';
 
 function App() {
-
-  return (
-    <div className='wrapper'>
-      <Header text="Pizza Day"></Header>
-      <nav className='nav'>
-        <NavLink to='/' className='nav_link'>Home</NavLink>
-        <NavLink to='/auth' className='nav_link'>Login</NavLink>
-        <NavLink to='/menu' className='nav_link'>Menu</NavLink>
-      </nav>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/auth' element={<Auth />} />
-        <Route path='/menu' element={<Menu />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </div>
-  )
+    return (
+        <div className="wrapper">
+            <Header text="Pizza Day"></Header>
+            <nav className="nav">
+                <NavLink to="/" className="nav_link">
+                    Home
+                </NavLink>
+                <NavLink to="/auth" className="nav_link">
+                    Login
+                </NavLink>
+                <NavLink to="/menu" className="nav_link">
+                    Menu
+                </NavLink>
+                <NavLink to="/order/new" className="nav_link">
+                    New Order
+                </NavLink>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/order/new" element={<NewOrder />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/pages/NewOrder.jsx
+++ src/pages/NewOrder.jsx
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema = yup.object().shape({
+    name: yup.string().required('field is required'),
+    number: yup
+        .string()
+        .matches(/^\+(?:[0-9] ?){6,14}[0-9]$/, 'phone number is invalid')
+        .required(),
+    address: yup
+        .string()
+        .min(7)
+        .required('address must be at least 7 characters'),
+});
 
 const NewOrder = () => {
-    return <div></div>;
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm({
+        mode: 'onBlur',
+        defaultValues: {
+            name: 'Vlad',
+            number: '+1234567890',
+            address: '',
+            priority: false,
+        },
+        resolver: yupResolver(schema),
+    });
+
+    // console.log(formState);
+
+    const handleFormSubmit = (data) => {
+        console.log(data);
+        reset();
+    };
+
+    return (
+        <div className="order">
+            <h1 className="order__heading">Ready to order? Let`s go!</h1>
+            <form
+                className="order__form"
+                onSubmit={handleSubmit(handleFormSubmit)}
+            >
+                <div className="order__field">
+                    <label htmlFor="name">First name</label>
+                    <input {...register('name')} type="text" id="name" />
+                    {errors.name && <span>{errors.name.message}</span>}
+                </div>
+                <div className="order__field">
+                    <label htmlFor="number">Phone number</label>
+                    <input {...register('number')} type="text" id="number" />
+                    {errors.number && <span>{errors.number.message}</span>}
+                </div>
+                <div className="order__field">
+                    <label htmlFor="address">Address</label>
+                    <input {...register('address')} type="text" id="address" />
+                    {errors.address && <span>{errors.address.message}</span>}
+                </div>
+                <div className="order__field checkbox">
+                    <input
+                        {...register('priority')}
+                        type="checkbox"
+                        id="priority"
+                    />
+                    <label htmlFor="priority">
+                        Want to yo give your order priority?
+                    </label>
+                </div>
+                <button className="order__button" type="submit">
+                    Order now for €39.00
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default NewOrder;
