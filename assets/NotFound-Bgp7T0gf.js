9fb47054727db94532e75728fdc61beba720a17c
Alishta <arnery1234@gmail.com>
2024-05-01 18:00:22 +0300

    Updates

--- assets/NotFound-Bgp7T0gf.js
+++
@@ -1,1 +0,0 @@
-import{j as n,L as t}from"./index-CcuHbi4q.js";const e=()=>n.jsxs("div",{children:[n.jsx("h1",{children:"Page not found"}),n.jsx(t,{to:"/",children:"Return to main page"})]});export{e as default};
