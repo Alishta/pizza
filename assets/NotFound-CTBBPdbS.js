7f3d7dbb577c819441668e7d980f6604885cf73a
Alishta <arnery1234@gmail.com>
2024-05-01 17:25:46 +0300

    Updates

---
+++ assets/NotFound-CTBBPdbS.js
@@ -0,0 +1,1 @@
+import{j as n,L as t}from"./index-4quhCTyG.js";const e=()=>n.jsxs("div",{children:[n.jsx("h1",{children:"Page not found"}),n.jsx(t,{to:"/",children:"Return to main page"})]});export{e as default};
