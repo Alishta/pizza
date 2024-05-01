273b129a615e7305ebe7d4618c4585ac0008847c
Alishta <arnery1234@gmail.com>
2024-05-01 17:53:42 +0300

    Updates

---
+++ assets/NotFound-Bgp7T0gf.js
@@ -0,0 +1,1 @@
+import{j as n,L as t}from"./index-CcuHbi4q.js";const e=()=>n.jsxs("div",{children:[n.jsx("h1",{children:"Page not found"}),n.jsx(t,{to:"/",children:"Return to main page"})]});export{e as default};
--- assets/NotFound-BtmLcG-2.js
+++
@@ -1,1 +0,0 @@
-import{j as n,L as t}from"./index-Dp5TG70L.js";const e=()=>n.jsxs("div",{children:[n.jsx("h1",{children:"Page not found"}),n.jsx(t,{to:"/",children:"Return to main page"})]});export{e as default};
