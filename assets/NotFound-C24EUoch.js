44280db144b7088b50e1e0eb94f2798f697f7803
Alishta <arnery1234@gmail.com>
2024-05-01 18:04:16 +0300

    Updates

---
+++ assets/NotFound-C24EUoch.js
@@ -0,0 +1,1 @@
+import{j as n,L as t}from"./index-Ds8cUp2H.js";const e=()=>n.jsxs("div",{children:[n.jsx("h1",{children:"Page not found"}),n.jsx(t,{to:"/pizza/",children:"Return to main page"})]});export{e as default};
