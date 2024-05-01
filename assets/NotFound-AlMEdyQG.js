f51a755b97f4d0570c07bba296f3a8dfe709a9b1
Alishta <arnery1234@gmail.com>
2024-05-01 18:09:02 +0300

    Updates

--- assets/NotFound-AlMEdyQG.js
+++
@@ -1,1 +0,0 @@
-import{j as n,L as t}from"./index-C12JN4Gs.js";const e=()=>n.jsxs("div",{children:[n.jsx("h1",{children:"Page not found"}),n.jsx(t,{to:"/pizza/",children:"Return to main page"})]});export{e as default};
